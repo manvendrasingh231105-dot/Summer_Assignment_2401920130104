ed4202e2fe44cf40a9248f59855f2fb539cf37c2
Manvendra Singh <manvendrasingh231105@gmail.com>
2026-06-02 12:45:47 +0530

    day_2

---
+++ Week_1/Day_2/Contains_Duplicate.java
@@ -0,0 +1,14 @@
+package Week_1.Day_2;
+
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        Set<Integer> s= new HashSet<>();
+        for (int n: nums){
+            s.add(n);
+
+        }
+        return  nums.length != s.size();
+
+        
+    }
+}
---
+++ Week_1/Day_2/Maximum_Subarray.java
@@ -0,0 +1,15 @@
+package Week_1.Day_2;
+
+class Solution {
+    public int maxSubArray(int[] nums) {
+         int r= nums[0];
+        int max= nums[0];
+
+        for (int i = 1; i < nums.length; i++) {
+            max=Math.max(max+nums[i],nums[i]);
+            r=Math.max(r,max);
+        }
+        return r;
+        
+    }
+}
